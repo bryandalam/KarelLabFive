1ed45b51e02daa9474d04cb387c9a496383ec041
bryandalam <bryandalam@gmail.cm>
2017-09-19 12:12:10 -0700

    Pair Programming

--- DoubleBot.java
+++ DoubleBot.java
@@ -13,6 +13,7 @@ public class DoubleBot extends Robot
 {
     // instance variables - replace the example below with your own
     public int beepers;
+    public int twiceTheBeepers;
 
     /**
      * Constructor for objects of class Template
@@ -23,7 +24,34 @@ public class DoubleBot extends Robot
 
     public void doubleBeepers()
     {
-        // put your code here
-        
+        findBeepers();
+        pickUpBeepers();
+        placeBeepers();
+        }
+    public void pickUpBeepers() {
+        while (nextToABeeper()) {
+            pickBeeper();
+            beepers++;
+        }
+    }
+    public void findBeepers() {
+        while (!nextToABeeper()) {
+            move();
+        }
+    }
+    public void placeBeepers() {
+        int i;
+        for (i = 0; i < beepers;  i++) {
+            putBeeper();
+        }
+        move();
+        twiceTheBeepers = beepers * 2;
+        for (i = 0; i < twiceTheBeepers; i++) {
+            putBeeper();
+        }
+        move();
     }
 }
+        
+        
+
