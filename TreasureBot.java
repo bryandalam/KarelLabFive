61fe5c20c699efffa36b1ff71ec2617d22c48de5
bryandalam <bryandalam@gmail.cm>
2017-09-19 12:50:19 -0700

    Working On #2

--- TreasureBot.java
+++ TreasureBot.java
@@ -10,10 +10,77 @@ public class TreasureBot extends Robot
     public TreasureBot(int st, int av, Direction dir, int numBeepers) {
         super(st, av, dir, numBeepers);
     }
+    public int beepers;
     
     public void findTreasure() {
-        
+        findBeeper();
+        followBeeper();
     }
-   
+    public void findBeeper(){
+        faceNorth();
+        while (!nextToABeeper()) {
+            move();
+        }
+    }
+    public void followBeeper() {
+        while (nextToABeeper()) {
+            pickBeeper();
+            beepers++;
+        }       
+        if (beepers == 1) {
+            goNorth();
+        }
+        if (beepers == 2) {
+            goEast();
+        }
+        if (beepers == 3) {
+            goSouth();
+        }
+        if (beepers == 4) {
+            goWest();
+        }
+        if (beepers == 5) {
+            turnOff();
+        }
+        beepers = 0;
+    }
+    public void goNorth() {
+        while (!facingNorth()) {
+            turnLeft();
+        }
+        while (!nextToABeeper()) {
+            move();
+        }
+    }
+    public void goEast() {
+        while (!facingEast()) {
+            turnLeft();
+        }
+        while (!nextToABeeper()) {
+            move();
+        }
+    }
+    public void goSouth() {
+        while (!facingSouth()) {
+            turnLeft();
+        }
+        while (!nextToABeeper()) {
+            move();
+            }
+        }
+    public void goWest() {
+        while (!facingWest()) {
+            turnLeft();
+        }
+        while (!nextToABeeper()) {
+            move();
+        }
+    }
+    public void faceNorth(){
+        while (!facingNorth()) {
+            turnLeft();
+        }
+    }
+    
 }
 
